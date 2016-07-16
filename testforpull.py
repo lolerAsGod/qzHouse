c8ea4e5ddd5e0c96105bd14d99954b0e38328239
lolerAsGod <1290573126@qq.com>
2016-07-16 21:23:41 +0800

    test

---
+++ testforpull.py
@@ -0,0 +1,17 @@
+import sys
+Onedimensionlist = []
+Twodimensionlist = []
+while True:
+    message = sys.stdin.readline()
+    if message == '\n':
+        break
+    else:
+        message.rstrip() #去除多余的括号
+        partlist = message.split()   #根据空格分裂成列表
+        Twodimensionlist.append(partlist)
+         #读取列表字符串并将字符串转换成列表
+for x in Twodimensionlist:
+    for y in x:
+        y = int(y) #二维列表中的元素转换成整数
+        Onedimensionlist.append(y) 
+print(Onedimensionlist)
